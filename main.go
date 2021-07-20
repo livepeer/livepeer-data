581c5c03a2f6d6732db218a23e714a589c7be766
Victor Elias <victorgelias@gmail.com>
2021-07-20 18:20:09 -0300

    Implement some basic stream health aggregation

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
+	"net/http"
 	"os"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/golang/glog"
@@ -36,6 +39,7 @@ func main() {
 	CheckErr(err)
 
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	startOffset := time.Now().Add(-5*time.Minute).UnixNano() / 1e6 // start consuming from 5 mins ago
 	msgs, err := consumer.Consume(ctx, streamName, event.ConsumeOptions{
 		StreamOptions: &event.StreamOptions{
@@ -55,34 +59,84 @@ func main() {
 	})
 	CheckErr(err)
 
+	streamHealths := sync.Map{}
+	go func() {
+		for msg := range msgs {
+			// json, err := json.Marshal(message.Properties)
+			var evt StreamHealthTranscodeEvent
+			err := json.Unmarshal(msg.Data[0], &evt)
+			CheckErr(err)
+
+			glog.Infof("received message. consumer=%q, offset=%d, seqNo=%d, startTimeAge=%q, latency=%q",
+				msg.Consumer.GetName(), msg.Consumer.GetOffset(), evt.Segment.SeqNo,
+				time.Since(time.Unix(0, evt.StartTime)), time.Duration(evt.LatencyMs)*time.Millisecond)
+
+			mid := evt.ManifestID
+			var health StreamHealthStatus
+			if saved, ok := streamHealths.Load(mid); ok {
+				health = saved.(StreamHealthStatus)
+			} else {
+				health = StreamHealthStatus{
+					ManifestID: mid,
+					Conditions: []*HealthCondition{
+						{Type: Transcoding},
+						{Type: RealTime},
+						{Type: NoErrors},
+					},
+				}
+			}
+			ts := time.Unix(0, evt.StartTime).Add(time.Duration(evt.LatencyMs))
+			for _, condition := range health.Conditions {
+				switch condition.Type {
+				case Transcoding:
+					condition.Update(ts, evt.Success)
+				case RealTime:
+					condition.Update(ts, evt.LatencyMs < int64(evt.Segment.Duration*1000))
+				case NoErrors:
+					noErrors := true
+					for _, attempt := range evt.Attempts {
+						noErrors = noErrors && attempt.Error == nil
+					}
+					condition.Update(ts, noErrors)
+				}
+			}
+			streamHealths.Store(mid, health)
+		}
+	}()
+
+	srv := &http.Server{Addr: ":8080"}
 	go func() {
 		defer cancel()
 		glog.Infoln("Stream name", streamName)
 		glog.Infoln("Press any key to stop")
 		bufio.NewReader(os.Stdin).ReadString('\n')
-		time.Sleep(200 * time.Millisecond)
+
+		shutx, cancel := context.WithTimeout(ctx, 1*time.Second)
+		defer cancel()
+		CheckErr(srv.Shutdown(shutx))
 	}()
 
-	for msg := range msgs {
-		// json, err := json.Marshal(message.Properties)
-		var v struct {
-			Segment struct {
-				SeqNo int
-			}
-			StartTime int64
-			LatencyMs int64
+	http.HandleFunc("/api/stream/health/", func(rw http.ResponseWriter, r *http.Request) {
+		parts := strings.SplitN(r.URL.Path, "/", 6)
+		if len(parts) != 5 {
+			rw.WriteHeader(http.StatusBadRequest)
+			return
 		}
-		err := json.Unmarshal(msg.Data[0], &v)
+		manifestID := parts[4]
+		healthIface, ok := streamHealths.Load(manifestID)
+		if !ok {
+			rw.WriteHeader(http.StatusNotFound)
+			return
+		}
+		health := healthIface.(StreamHealthStatus)
+		rw.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(rw).Encode(health); err != nil {
+			glog.Errorf("Error writing stream health JSON response. err=%q", err)
+		}
+	})
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		CheckErr(err)
-
-		glog.Infof("received message. consumer=%q, offset=%d, seqNo=%d, startTimeAge=%q, latency=%q",
-			msg.Consumer.GetName(), msg.Consumer.GetOffset(), v.Segment.SeqNo,
-			time.Since(time.Unix(0, v.StartTime)), time.Duration(v.LatencyMs)*time.Millisecond)
-		// err := consumerContext.Consumer.StoreOffset()
-		// CheckErr(err)
-
-		// TODO: now use these events for something useful like building stream
-		// health state in memory.
 	}
 }
 
@@ -90,4 +144,66 @@ func CheckErr(err error) {
 	if err != nil {
 		glog.Fatalln("error", err)
 	}
+}
+
+type OrchestratorMetadata struct {
+	Address       string `json:"address"`
+	TranscoderUri string `json:"transcodeUri"`
+}
+
+type TranscodeAttemptInfo struct {
+	Orchestrator OrchestratorMetadata `json:"orchestrator"`
+	LatencyMs    int64                `json:"latencyMs"`
+	Error        *string              `json:"error"`
+}
+
+type SegmentMetadata struct {
+	Name     string  `json:"name"`
+	SeqNo    uint64  `json:"seqNo"`
+	Duration float64 `json:"duration"`
+}
+
+type StreamHealthTranscodeEvent struct {
+	NodeID     string                 `json:"nodeId"`
+	ManifestID string                 `json:"manifestId"`
+	Segment    SegmentMetadata        `json:"segment"`
+	StartTime  int64                  `json:"startTime"`
+	LatencyMs  int64                  `json:"latencyMs"`
+	Success    bool                   `json:"success"`
+	Attempts   []TranscodeAttemptInfo `json:"attempts"`
+}
+
+type HealthConditionType string
+
+const (
+	Transcoding HealthConditionType = "Transcoding"
+	RealTime    HealthConditionType = "RealTime"
+	NoErrors    HealthConditionType = "NoErrors"
+)
+
+// type StatusFrequency struct {
+// 	LastMinute    float64
+// 	Last10Minutes float64
+// 	LastHour      float64
+// }
+
+type HealthCondition struct {
+	Type   HealthConditionType
+	Status *bool
+	// Frequency          *StatusFrequency
+	LastProbeTime      time.Time
+	LastTransitionTime time.Time
+}
+
+func (c *HealthCondition) Update(now time.Time, status bool) {
+	c.LastProbeTime = now
+	if c.Status == nil || *c.Status != status {
+		c.LastTransitionTime = now
+		c.Status = &status
+	}
+}
+
+type StreamHealthStatus struct {
+	ManifestID string
+	Conditions []*HealthCondition
 }
