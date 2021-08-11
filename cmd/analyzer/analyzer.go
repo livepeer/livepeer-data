0ebdad135c65c1e8d224597377c2fcb7bc46cef9
Victor Elias <victorgelias@gmail.com>
2021-08-11 17:52:36 -0300

    cmd/analyzer: Make cli flags kebab-case
    This will create nicer envs with _ separators.

--- cmd/analyzer/analyzer.go
+++ cmd/analyzer/analyzer.go
@@ -38,24 +38,24 @@ var (
 func init() {
 	fs := flag.NewFlagSet("analyzer", flag.ExitOnError)
 
-	fs.StringVar(&rabbitmqUri, "rabbitmqUri", "amqp://guest:guest@localhost:5672/livepeer", "URI for RabbitMQ server to consume from. Can be specified as a default AMQP URI which will be converted to stream protocol.")
-	fs.StringVar(&amqpUri, "amqpUri", "", "Explicit AMQP URI in case of non-default protocols/ports (optional). Must point to the same cluster as rabbitmqUri")
+	fs.StringVar(&rabbitmqUri, "rabbitmq-uri", "amqp://guest:guest@localhost:5672/livepeer", "URI for RabbitMQ server to consume from. Can be specified as a default AMQP URI which will be converted to stream protocol.")
+	fs.StringVar(&amqpUri, "amqp-uri", "", "Explicit AMQP URI in case of non-default protocols/ports (optional). Must point to the same cluster as rabbitmqUri")
 
-	fs.StringVar(&golivepeerExchange, "golivepeerExchange", "lp_golivepeer_metadata", "Name of RabbitMQ exchange to bind the stream to on creation")
-	fs.StringVar(&shardPrefixesFlag, "shardPrefixes", "", "Comma-separated list of prefixes of manifest IDs to process events from")
+	fs.StringVar(&golivepeerExchange, "golivepeer-exchange", "lp_golivepeer_metadata", "Name of RabbitMQ exchange to bind the stream to on creation")
+	fs.StringVar(&shardPrefixesFlag, "shard-prefixes", "", "Comma-separated list of prefixes of manifest IDs to process events from")
 
 	// Server options
 	fs.StringVar(&serverOpts.Host, "host", "localhost", "Hostname to bind to")
 	fs.UintVar(&serverOpts.Port, "port", 8080, "Port to listen on")
-	fs.StringVar(&serverOpts.APIRoot, "apiRoot", "/data", "Root path where to bind the API to")
-	fs.DurationVar(&serverOpts.ShutdownGracePeriod, "shutdownGracePerod", 15*time.Second, "Grace period to wait for server shutdown before using the force")
+	fs.StringVar(&serverOpts.APIRoot, "api-root", "/data", "Root path where to bind the API to")
+	fs.DurationVar(&serverOpts.ShutdownGracePeriod, "shutdown-grace-perod", 15*time.Second, "Grace period to wait for server shutdown before using the force")
 
 	// Streaming options
-	fs.StringVar(&streamingOpts.Stream, "rabbitmqStreamName", "lp_stream_health_v0", "Name of RabbitMQ stream to create and consume from")
-	fs.StringVar(&streamingOpts.ConsumerName, "consumerName", "", `Consumer name to use when consuming stream (default "analyzer-${hostname}")`)
-	fs.StringVar(&streamingOpts.MaxLengthBytes, "streamMaxLength", "50gb", "When creating a new stream, config for max total storage size")
-	fs.StringVar(&streamingOpts.MaxSegmentSizeBytes, "streamMaxSegmentSize", "500mb", "When creating a new stream, config for max stream segment size in storage")
-	fs.DurationVar(&streamingOpts.MaxAge, "streamMaxAge", 30*24*time.Hour, `When creating a new stream, config for max age of stored events`)
+	fs.StringVar(&streamingOpts.Stream, "rabbitmq-stream-name", "lp_stream_health_v0", "Name of RabbitMQ stream to create and consume from")
+	fs.StringVar(&streamingOpts.ConsumerName, "consumer-name", "", `Consumer name to use when consuming stream (default "analyzer-${hostname}")`)
+	fs.StringVar(&streamingOpts.MaxLengthBytes, "stream-max-length", "50gb", "When creating a new stream, config for max total storage size")
+	fs.StringVar(&streamingOpts.MaxSegmentSizeBytes, "stream-max-segment-size", "500mb", "When creating a new stream, config for max stream segment size in storage")
+	fs.DurationVar(&streamingOpts.MaxAge, "stream-max-age", 30*24*time.Hour, `When creating a new stream, config for max age of stored events`)
 
 	flag.Set("logtostderr", "true")
 	glogVFlag := flag.Lookup("v")
