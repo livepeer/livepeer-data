8a18aec95a8fe0864d6e3ea234fa9dd6057d9c68
Victor Elias <victorgelias@gmail.com>
2021-09-02 18:31:20 -0300

    pkg/data: Create MistStreamStats event

---
+++ pkg/data/mistStreamStats.go
@@ -0,0 +1,31 @@
+package data
+
+const EventTypeMistStreamStats EventType = "mist_stream_stats"
+
+func NewMistStreamStatsEvent(nodeID, streamID string, viewerCount int, mediaTimeMs int64, multistream []MultistreamTargetStats) (*MistStreamStatsEvent, error) {
+	return &MistStreamStatsEvent{
+		Base:        newEventBase(EventTypeMistStreamStats, streamID),
+		NodeID:      nodeID,
+		ViewerCount: viewerCount,
+		MediaTimeMs: mediaTimeMs,
+		Multistream: multistream,
+	}, nil
+}
+
+type MistStreamStatsEvent struct {
+	Base
+	NodeID string `json:"nodeId"`
+
+	ViewerCount int   `json:"viewerCount"`
+	MediaTimeMs int64 `json:"mediaTimeMs"`
+
+	Multistream []MultistreamTargetStats
+}
+
+type MultistreamTargetStats struct {
+	Target MultistreamTargetInfo `json:"target"`
+
+	ActiveSeconds int64 `json:"activeSeconds"`
+	Bytes         int64 `json:"bytes"`
+	MediaTimeMs   int64 `json:"mediaTimeMs"`
+}
