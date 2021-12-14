4c9427e2833dff586b42a4d993bc8b98f0c1c1de
Victor Elias <victorgelias@gmail.com>
2021-12-14 16:34:40 -0300

    monitor: Create monitor helper package

---
+++ monitor/http.go
@@ -0,0 +1,38 @@
+package monitor
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	httpReqDuration = Factory.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: MetricName("http_request_duration_sec"),
+			Help: "Request duration of HTTP requests in seconds.",
+		},
+		[]string{"code", "method", "api"},
+	)
+	httpReqTimeToHeaders = Factory.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: MetricName("http_request_time_to_headers_sec"),
+			Help: "Time until HTTP headers are written, in seconds.",
+		},
+		[]string{"code", "method", "api"},
+	)
+)
+
+func ObservedHandler(apiName string, handler http.HandlerFunc) http.Handler {
+	if !inited {
+		return handler
+	}
+	handler = promhttp.InstrumentHandlerTimeToWriteHeader(
+		httpReqTimeToHeaders.MustCurryWith(prometheus.Labels{"api": apiName}),
+		handler)
+	handler = promhttp.InstrumentHandlerDuration(
+		httpReqDuration.MustCurryWith(prometheus.Labels{"api": apiName}),
+		handler)
+	return handler
+}
---
+++ monitor/monitor.go
@@ -0,0 +1,26 @@
+package monitor
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	Namespace = "livepeer"
+	Subsystem = "analyzer"
+	Factory   promauto.Factory
+
+	inited bool
+)
+
+func Init() {
+	if inited {
+		panic("can only init monitor once")
+	}
+	inited = true
+	Factory = promauto.With(prometheus.DefaultRegisterer)
+}
+
+func MetricName(name string) string {
+	return prometheus.BuildFQName(Namespace, Subsystem, name)
+}
