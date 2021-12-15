2ee7bc6d63596ccfb660ba0b0fde99cc52236f6a
Victor Elias <victorgelias@gmail.com>
2021-12-14 22:18:19 -0300

    monitor: Add in-flight gauge to API monitoring

--- monitor/http.go
+++ monitor/http.go
@@ -11,17 +11,26 @@ var (
 	httpReqDuration = Factory.NewSummaryVec(
 		prometheus.SummaryOpts{
 			Name: MetricName("http_request_duration_sec"),
-			Help: "Request duration of HTTP requests in seconds.",
+			Help: "Request duration of HTTP requests in seconds",
 		},
 		[]string{"code", "method", "api"},
 	)
 	httpReqTimeToHeaders = Factory.NewSummaryVec(
 		prometheus.SummaryOpts{
 			Name: MetricName("http_request_time_to_headers_sec"),
-			Help: "Time until HTTP headers are written, in seconds.",
+			Help: "Time until HTTP headers are written, in seconds",
 		},
 		[]string{"code", "method", "api"},
 	)
+	httpReqInFlight = func(api string) prometheus.Gauge {
+		return Factory.NewGauge(
+			prometheus.GaugeOpts{
+				Name:        MetricName("http_request_in_flight"),
+				Help:        "Number of current requests in-flight for the specific API",
+				ConstLabels: prometheus.Labels{"api": api},
+			},
+		)
+	}
 )
 
 func ObservedHandler(apiName string, handler http.Handler) http.Handler {
@@ -33,6 +42,9 @@ func ObservedHandler(apiName string, handler http.Handler) http.Handler {
 		handler)
 	handler = promhttp.InstrumentHandlerDuration(
 		httpReqDuration.MustCurryWith(prometheus.Labels{"api": apiName}),
+		handler)
+	handler = promhttp.InstrumentHandlerInFlight(
+		httpReqInFlight(apiName),
 		handler)
 	return handler
 }
