0dac81ef6e8db057ed3f1886c3eba4ce85107f97
hjpotter92 <git@hjpotter92.email>
2022-01-17 15:04:05 +0530

    Add support for mist runtime to identify binaries.

--- cmd/analyzer/analyzer.go
+++ cmd/analyzer/analyzer.go
@@ -2,6 +2,7 @@ package analyzer
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"os"
 	"os/signal"
@@ -22,7 +23,26 @@ import (
 type BuildFlags struct {
 	Version string
 }
+
+type MistOptional struct {
+	Name   string `json:"name"`
+	Help   string `json:"help"`
+	Option string `json:"option,omitempty"`
+	// Short   string `json:"short"`
+	Default string `json:"default,omitempty"`
+}
+
+type MistConfig struct {
+	Name         string         `json:"name"`
+	Description  string         `json:"desc"`
+	FriendlyName string         `json:"friendly"`
+	Optional     []MistOptional `json:"optional,omitempty"`
+	Version      string         `json:"version,omitempty"`
+}
+
 type cliFlags struct {
+	json bool
+
 	rabbitmqUri string
 	amqpUri     string
 
@@ -30,14 +50,16 @@ type cliFlags struct {
 	shardPrefixesFlag  string
 	shardPrefixes      []string
 
-	serverOpts       api.ServerOptions
+	ServerOpts       api.ServerOptions `json:"server"`
 	streamingOpts    health.StreamingOptions
 	memoryRecordsTtl time.Duration
 }
 
-func parseFlags() cliFlags {
+func parseFlags(version string) cliFlags {
 	cli := cliFlags{}
 	fs := flag.NewFlagSet("analyzer", flag.ExitOnError)
+
+	fs.BoolVar(&cli.json, "json", false, "Print application info as json")
 
 	fs.StringVar(&cli.rabbitmqUri, "rabbitmq-uri", "amqp://guest:guest@localhost:5672/livepeer", "URI for RabbitMQ server to consume from. Can be specified as a default AMQP URI which will be converted to stream protocol.")
 	fs.StringVar(&cli.amqpUri, "amqp-uri", "", "Explicit AMQP URI in case of non-default protocols/ports (optional). Must point to the same cluster as rabbitmqUri")
@@ -46,15 +68,15 @@ func parseFlags() cliFlags {
 	fs.StringVar(&cli.shardPrefixesFlag, "shard-prefixes", "", "Comma-separated list of prefixes of manifest IDs to process events from")
 
 	// Server options
-	fs.StringVar(&cli.serverOpts.Host, "host", "localhost", "Hostname to bind to")
-	fs.UintVar(&cli.serverOpts.Port, "port", 8080, "Port to listen on")
-	fs.DurationVar(&cli.serverOpts.ShutdownGracePeriod, "shutdown-grace-perod", 15*time.Second, "Grace period to wait for server shutdown before using the force")
+	fs.StringVar(&cli.ServerOpts.Host, "host", "localhost", "Hostname to bind to")
+	fs.UintVar(&cli.ServerOpts.Port, "port", 8080, "Port to listen on")
+	fs.DurationVar(&cli.ServerOpts.ShutdownGracePeriod, "shutdown-grace-perod", 15*time.Second, "Grace period to wait for server shutdown before using the force")
 	// API Handler
-	fs.StringVar(&cli.serverOpts.APIRoot, "api-root", "/data", "Root path where to bind the API to")
-	fs.BoolVar(&cli.serverOpts.Prometheus, "prometheus", false, "Whether to enable Prometheus metrics registry and expose /metrics endpoint")
-	fs.StringVar(&cli.serverOpts.AuthURL, "auth-url", "", "Endpoint for an auth server to call for both authentication and authorization of API calls")
-	fs.StringVar(&cli.serverOpts.OwnRegion, "own-region", "", "Identifier of the region where the service is running, used for triggering global request proxying")
-	fs.StringVar(&cli.serverOpts.RegionalHostFormat, "regional-host-format", "localhost", "Format to build regional URL for proxying to other regions. Should contain 1 %s directive where the region will be replaced (e.g. %s.livepeer.monster)")
+	fs.StringVar(&cli.ServerOpts.APIRoot, "api-root", "/data", "Root path where to bind the API to")
+	fs.BoolVar(&cli.ServerOpts.Prometheus, "prometheus", false, "Whether to enable Prometheus metrics registry and expose /metrics endpoint")
+	fs.StringVar(&cli.ServerOpts.AuthURL, "auth-url", "", "Endpoint for an auth server to call for both authentication and authorization of API calls")
+	fs.StringVar(&cli.ServerOpts.OwnRegion, "own-region", "", "Identifier of the region where the service is running, used for triggering global request proxying")
+	fs.StringVar(&cli.ServerOpts.RegionalHostFormat, "regional-host-format", "localhost", "Format to build regional URL for proxying to other regions. Should contain 1 %s directive where the region will be replaced (e.g. %s.livepeer.monster)")
 
 	// Streaming options
 	fs.StringVar(&cli.streamingOpts.Stream, "rabbitmq-stream-name", "lp_stream_health_v0", "Name of RabbitMQ stream to create and consume from")
@@ -84,12 +106,33 @@ func parseFlags() cliFlags {
 	if cli.shardPrefixesFlag != "" {
 		cli.shardPrefixes = strings.Split(cli.shardPrefixesFlag, ",")
 	}
+
+	if cli.json {
+		data := &MistConfig{
+			Name:         "LivepeerAnalyzer",
+			Version:      version,
+			Description:  "",
+			FriendlyName: "Livepeer Analyzer",
+			Optional:     []MistOptional{},
+		}
+		fs.VisitAll(func(f *flag.Flag) {
+			data.Optional = append(data.Optional, MistOptional{
+				Name:    f.Name,
+				Help:    f.Usage,
+				Default: f.DefValue,
+			})
+		})
+		b, _ := json.Marshal(data)
+		os.Stdout.Write(b)
+		os.Exit(255)
+	}
+
 	return cli
 }
 
 func Run(build BuildFlags) {
-	cli := parseFlags()
-	cli.serverOpts.APIHandlerOptions.ServerName = "analyzer/" + build.Version
+	cli := parseFlags(build.Version)
+	cli.ServerOpts.APIHandlerOptions.ServerName = "analyzer/" + build.Version
 
 	glog.Infof("Stream health care system starting up... version=%q", build.Version)
 	ctx := contextUntilSignal(context.Background(), syscall.SIGINT, syscall.SIGTERM)
@@ -115,7 +158,7 @@ func Run(build BuildFlags) {
 	}
 
 	glog.Info("Starting server...")
-	err = api.ListenAndServe(ctx, cli.serverOpts, healthcore)
+	err = api.ListenAndServe(ctx, cli.ServerOpts, healthcore)
 	if err != nil {
 		glog.Fatalf("Error starting api server. err=%q", err)
 	}
