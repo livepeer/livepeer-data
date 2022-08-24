8f2d3a1e2643c415e2b6609e6e15e1bc25329d35
Victor Elias <victorgelias@gmail.com>
2022-08-24 18:28:34 -0300

    views: Create views client with start views query

---
+++ views/client.go
@@ -0,0 +1,58 @@
+package views
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+	livepeer "github.com/livepeer/go-api-client"
+	prometheus "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
+)
+
+type TotalViews struct {
+	ID    string `json:"id"`
+	Start int64  `json:"start"`
+}
+
+type Client struct {
+	prom prometheus.API
+	lp   livepeer.Client
+}
+
+func (c *Client) GetTotalViews(ctx context.Context, id string) ([]TotalViews, error) {
+	asset, err := c.lp.GetAsset(id)
+	if errors.Is(err, livepeer.ErrNotExists) {
+		asset, err = c.lp.GetAssetByPlaybackID(id, false)
+	}
+	if errors.Is(err, livepeer.ErrNotExists) {
+		return nil, errors.New("asset not found")
+	} else if err != nil {
+		return nil, fmt.Errorf("error getting asset: %w", err)
+	}
+
+	value, warn, err := c.prom.Query(ctx, startViewsQuery(asset), time.Time{})
+	if len(warn) > 0 {
+		glog.Warningf("Prometheus query warnings: %q", warn)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("query error: %w", err)
+	}
+	if value.Type() != model.ValScalar {
+		return nil, fmt.Errorf("unexpected value type: %s", value.Type())
+	}
+
+	return []TotalViews{{
+		ID:    asset.PlaybackID,
+		Start: int64(value.(*model.Scalar).Value),
+	}}, nil
+}
+
+func startViewsQuery(asset *livepeer.Asset) string {
+	return fmt.Sprintf(
+		`sum(increase(mist_viewcount{stream=~"video(rec)?\\+%s"} [1y]))`,
+		asset.PlaybackID,
+	)
+}
