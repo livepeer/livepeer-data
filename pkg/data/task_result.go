ce634bb0de25e40cadd3f8f5a64a21dd3bc490af
Rafał Leszko <rafal@livepeer.org>
2023-02-10 08:49:35 +0100

    Changes after code review

--- pkg/data/task_result.go
+++ pkg/data/task_result.go
@@ -35,18 +35,14 @@ type TranscodeTaskOutput struct {
 	Asset UploadTaskOutput `json:"asset,omitempty"`
 }
 
-type TranscodeFileTaskOutputMp4 struct {
-	Renditions []TranscodeFileTaskOutputPath `json:"renditions,omitempty"`
-}
-
 type TranscodeFileTaskOutputPath struct {
 	Path string `json:"path,omitempty"`
 }
 
 type TranscodeFileTaskOutput struct {
-	BaseUrl string                      `json:"baseUrl,omitempty"`
-	Hls     *TranscodeFileTaskOutputPath `json:"hls,omitempty"`
-	Mp4     *TranscodeFileTaskOutputMp4  `json:"mp4,omitempty"`
+	BaseUrl string                        `json:"baseUrl,omitempty"`
+	Hls     *TranscodeFileTaskOutputPath  `json:"hls,omitempty"`
+	Mp4     []TranscodeFileTaskOutputPath `json:"mp4,omitempty"`
 }
 
 type UploadTaskOutput struct {
